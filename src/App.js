7c49c685f383cd83675f84352d1393eeffda4392
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2023-06-25 14:58:39 +0300

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {WWComponent} from "fs-comps-draft-yzhy";
+
+function App() {
+  return (
+    <div className="App">
+      <WWComponent/>
+    </div>
+  );
+}
+
+export default App;
